1353b783fc3127379c3bc04351fa4906fb6d1c54
Matias Fernandez <matifer.dev@gmail.com>
2024-03-17 12:54:43 -0300

    handleClick moved to different functions

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,42 @@ function App() {
   const [display, setDisplay] = useState('0');
   console.log('Display state', display);
 
-  const handleClick = (e) => {
-    let value = e.target.textContent;
-    if (value === 'AC') {
-      setDisplay('0');
-    } else if (value === 'C') {
-      setDisplay((prevDisplay) => {
-        if (prevDisplay.length === 1) return '0';
-        return prevDisplay.slice(0, -1);
-      });
-    } else if (/[0-9]/.test(value)) {
-      setDisplay((prevDisplay) => {
-        if (prevDisplay === '0') return value;
-        return display + value;
-      });
-    } else if (/[/*\-+]/.test(value)) {
-      setDisplay((prevDisplay) => {
-        if (/[/*\-+]$/.test(prevDisplay)) {
-          return prevDisplay.slice(0, -1) + value;
-        } else {
-          return prevDisplay + value;
-        }
-      });
-    } else if (value === '=') {
-      setDisplay(String(eval(display)));
-    }
+  const handleClear = () => {
+    setDisplay('0');
+  };
+
+  const handleClearOne = () => {
+    setDisplay((prevDisplay) => {
+      if (prevDisplay.length === 1) return '0';
+      return prevDisplay.slice(0, -1);
+    });
+  };
+
+  const handleNumber = (e) => {
+    const value = e.target.textContent;
+    setDisplay((prevDisplay) => {
+      if (prevDisplay === '0') return value;
+      return display + value;
+    });
+  };
+
+  const handleOperator = (e) => {
+    const value = e.target.textContent;
+    setDisplay((prevDisplay) => {
+      if (/[/*\-+]$/.test(prevDisplay)) {
+        return prevDisplay.slice(0, -1) + value;
+      } else {
+        return prevDisplay + value;
+      }
+    });
+  };
+
+  const handleEquals = () => {
+    setDisplay(String(eval(display)));
+  };
+
+  const handleDecimal = () => {
+    //
   };
 
   return (
@@ -37,58 +48,58 @@ function App() {
       <h1>JS Calculator</h1>
       <div className="calculator">
         <div id="display">{display}</div>
-        <div onClick={handleClick} id="clear">
+        <div onClick={handleClear} id="clear">
           AC
         </div>
-        <div onClick={handleClick} id="clear-one">
+        <div onClick={handleClearOne} id="clear-one">
           C
         </div>
-        <div onClick={handleClick} id="divide" className="operator">
+        <div onClick={handleOperator} id="divide" className="operator">
           /
         </div>
-        <div onClick={handleClick} id="multiply" className="operator">
+        <div onClick={handleOperator} id="multiply" className="operator">
           *
         </div>
-        <div onClick={handleClick} id="seven" className="number">
+        <div onClick={handleNumber} id="seven" className="number">
           7
         </div>
-        <div onClick={handleClick} id="eight" className="number">
+        <div onClick={handleNumber} id="eight" className="number">
           8
         </div>
-        <div onClick={handleClick} id="nine" className="number">
+        <div onClick={handleNumber} id="nine" className="number">
           9
         </div>
-        <div onClick={handleClick} id="subtract" className="operator">
+        <div onClick={handleOperator} id="subtract" className="operator">
           -
         </div>
-        <div onClick={handleClick} id="four" className="number">
+        <div onClick={handleNumber} id="four" className="number">
           4
         </div>
-        <div onClick={handleClick} id="five" className="number">
+        <div onClick={handleNumber} id="five" className="number">
           5
         </div>
-        <div onClick={handleClick} id="six" className="number">
+        <div onClick={handleNumber} id="six" className="number">
           6
         </div>
-        <div onClick={handleClick} id="add" className="operator">
+        <div onClick={handleOperator} id="add" className="operator">
           +
         </div>
-        <div onClick={handleClick} id="one" className="number">
+        <div onClick={handleNumber} id="one" className="number">
           1
         </div>
-        <div onClick={handleClick} id="two" className="number">
+        <div onClick={handleNumber} id="two" className="number">
           2
         </div>
-        <div onClick={handleClick} id="three" className="number">
+        <div onClick={handleNumber} id="three" className="number">
           3
         </div>
-        <div onClick={handleClick} id="equals">
+        <div onClick={handleEquals} id="equals">
           =
         </div>
-        <div onClick={handleClick} id="zero" className="number">
+        <div onClick={handleNumber} id="zero" className="number">
           0
         </div>
-        <div onClick={handleClick} id="decimal" className="number">
+        <div onClick={handleDecimal} id="decimal" className="number">
           .
         </div>
       </div>
